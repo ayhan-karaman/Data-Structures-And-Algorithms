8ad2b3a4d85f82205aa69920a587cf82cd5bba60
Ayhan2860 <Ayhan28.ak@outlook.com.tr>
2022-10-24 09:05:24 +0300

    Big O, Recursion, Binary Search Iterative and Recursive method lesson completed

---
+++ Search/Search/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Search;
+public class BinarySearch
+{
+    public static int Search<T>(T[] array, T key)
+    where T : IComparable
+    {
+        return search(array, 0, array.Length-1, key);
+    }
+    private static int search<T>(T[] array, int i, int j, T key)
+    where T : IComparable
+    {
+        while (true)
+        {
+            if(i == j)
+            {
+                if(array[i].Equals(key))
+                    return i;
+                return -1;
+            }
+            
+            int middle = (i + j) / 2;
+            if(array[middle].Equals(key))
+                return middle;
+            if(key.CompareTo(array[middle]) == -1)
+            {
+                 j = middle;
+                 continue;
+            }
+            i = middle +1;
+        }
+    }
+}
---
+++ UIs/SearchApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using Search;
+int[] arr = {10, 20, 30, 40, 50, 60, 70, 80};
+int index = BinarySearch.Search(arr, 300);
+if(index != -1)
+    Console.WriteLine($"Array[{index}] => {arr[index]}");
+else 
+    Console.WriteLine("Eleman bulunamadı!");
